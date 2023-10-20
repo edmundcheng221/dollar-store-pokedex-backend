cbf6a0660dd230d9bd6fed9abf4bc3e69e4809a0
edmundcheng221 <ec3219@nyu.edu>
2023-10-20 13:36:01 -0400

    Update router

--- README.md
+++ README.md
@@ -5,5 +5,5 @@ Backend application to serve pokedex data with GraphQL
 # Run locally
 
 ```code
-$ cd cmd && go run server.go
+$ go run server.go
 ```
---
+++ router/router.go
@@ -0,0 +1,16 @@
+package router
+
+import (
+	"net/http"
+
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/edmundcheng221/dollar-store-pokedex-backend/graph"
+)
+
+func InitializeRoutes() {
+	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}}))
+
+	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	http.Handle("/query", srv)
+}
